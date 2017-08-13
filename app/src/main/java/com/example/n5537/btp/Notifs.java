d6643bf73c0568fbb9b23d41f48613883f3c128b
Mohit <mohit21061996@gmail.com>
2017-08-13 21:26:21 +0530

    commit

---
+++ app/src/main/java/com/example/n5537/btp/Notifs.java
@@ -0,0 +1,22 @@
+package com.example.n5537.btp;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class Notifs extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_notifs);
+
+//        Intent intent = getIntent();
+//        String message = intent.getStringExtra(MainActivity.EXTRA_MESSAGE);
+
+        // Capture the layout's TextView and set the string as its text
+//        TextView textView = (TextVieww) findViewById(R.id.textView);
+//        textView.setText(message);
+    }
+}
